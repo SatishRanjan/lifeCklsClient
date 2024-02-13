609e5fcadb89866fa795f41065f19ad1af64ced2
SatishRanjan <satishranjan.github@gmail.com>
2024-02-12 16:39:56 -0800

    Azure Mongo DB URL

--- src/common/config.js
+++ src/common/config.js
@@ -1,5 +1,6 @@
 // Replace the URL with your localhost API endpoint
-const apiRoot = "http://localhost:8001"
+//const apiRoot = "http://localhost:8001"
+const apiRoot = "https://lifecklsservice.azurewebsites.net"
 
 module.exports = {
     registrationUrl: `${apiRoot}/v1/user/register`,
