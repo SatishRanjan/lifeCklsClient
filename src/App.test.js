5db835d30a8c0b912be8b9ab198e8a0b4630a87c
SatishRanjan <satishranjan.github@gmail.com>
2024-02-19 22:27:32 -0800

    Fixing the test text

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Welcome to the LifeCksls!/i);
+  const linkElement = screen.getByText(/Welcome to the LifeCkls!/i);
   expect(linkElement).toBeInTheDocument();
 });
